2965f41da889b9b61f9a7b42761e28f8c8fc1787
Carlos Abel Demirovic <carlos@stickerapp.com>
2018-11-01 16:11:44 +0100

    adding components and sass

---
+++ starterkit/src/components/header/header.js
@@ -0,0 +1,16 @@
+import {Component} from "react";
+import React from "react";
+import './header.scss';
+
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header-wrapper">
+                    <p>This is the header</p>
+            </div>
+        );
+    }
+}
+
+export default Header;
